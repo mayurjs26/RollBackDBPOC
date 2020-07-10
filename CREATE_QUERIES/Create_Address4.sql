1f868538c8d0906ac9748b5957670c449951cade
Shinde <mayur.b.shinde@capgemini.com>
2020-07-10 21:04:28 +0530

    commit

--- CREATE_QUERIES/Create_Address4.sql
+++ CREATE_QUERIES/Create_Address4.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `customer`.`adresss8` (
-  `id` INT NOT NULL,
+  `id` INT NOT NLL,
   `name` VARCHAR(45) NULL,
   `type` VARCHAR(45) NULL,
-  PRIMARY KEY (`id`))
+  PRIMARY KEY (`id`));
